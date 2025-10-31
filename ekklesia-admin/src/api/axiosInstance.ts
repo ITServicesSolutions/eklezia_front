a5de40427f98f1f09f12c331de5c9982393473d4
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2025-10-31 18:00:00 +0100

    add de l'url back-end

--- ekklesia-admin/src/api/axiosInstance.ts
+++ ekklesia-admin/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1', // Replace with your actual API base URL
+  baseURL: 'http://localhost:8000', // Replace with your actual API base URL
 });
 
 axiosInstance.interceptors.request.use(
